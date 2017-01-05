d9717cdc73592bdf1ad9b411b4a6c67cc315d009
Ilya91 <webmaster.1991@mail.ru>
2017-01-05 18:18:57 +0300

    protected $watermark; protected $path;

--- app/Photo.php
+++ app/Photo.php
@@ -8,8 +8,22 @@ class Photo extends Model
 {
     protected $fillable = ['file'];
     protected $uploads = '/images/';
+    protected $watermark;
+    protected $path;
+
+    public function getWatermark(){
+        return $this->watermark = public_path() . '/images/watermark.png';
+    }
+
+    public function getPath(){
+        return $this->path = public_path() . '/images/';
+    }
 
     public function getFileAttribute($photo){ // перезапись значения в столбце file таблицы photos
         return $this->uploads . $photo;
+    }
+
+    public function post(){
+        return $this->belongsTo('App\Post');
     }
 }
