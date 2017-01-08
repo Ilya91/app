f15353f218c93c4816303c1b3dd89dda47423799
Ilya91 <webmaster.1991@mail.ru>
2017-01-08 20:12:48 +0300

    search

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Comment;
 use App\Post;
 use Illuminate\Http\Request;
 
@@ -15,11 +15,37 @@ class BlogPostController extends Controller
             ]);
     }
 
-    public function show($id){
-        $post = Post::findOrFail($id);
+
+    public function search(Request $request){
+        $search = $request->input('search');
+        $posts = Post::where('title','like','%'.$search.'%')->orderBy('id')->paginate(10);
+        return view('default.index',
+            [
+                'posts' => $posts
+            ]);
+    }
+
+    public function posts(){
+        $posts = Post::orderBy('id', 'desc')->paginate(10);
+        return view('default.posts',
+            [
+                'posts' => $posts
+            ]);
+    }
+
+    /**
+     * @param $slug
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show($slug){
+        $post = Post::findBySlugOrFail($slug);
+        $comments = Comment::with('children')->where('post_id', $post->id)->where('parent_id', 0)->get();
+        //dd($comments);
+        $post = Post::findBySlugOrFail($slug);
         return view('default.post',
             [
-                'post' => $post
+                'post' => $post,
+                'comments' => $comments
             ]);
     }
 }
